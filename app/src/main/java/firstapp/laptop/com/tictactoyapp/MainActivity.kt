db34b87fd0f26a7a84eacb0c7651f22202b58de0
laptop <laptop@example.com>
2017-11-14 14:37:28 +0200

    change Some File In .ignore .

--- app/src/main/java/firstapp/laptop/com/tictactoyapp/MainActivity.kt
+++ app/src/main/java/firstapp/laptop/com/tictactoyapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    /*commit the offline Tag S*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
